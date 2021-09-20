908897f4f5e215f8b6bfa7deb9145080e42110b4
Steven Del Rosario <stevendelro@pm.me>
2021-09-20 15:52:11 -0500

    added comments

--- pages/api/foreca/getWeather/[...location].js
+++ pages/api/foreca/getWeather/[...location].js
@@ -1,27 +1,50 @@
 import axios from 'axios'
-import Cookies from 'universal-cookie'
 
-// In this file, two consecutive API calls are made:
+// In this file:
 //  1. GET: Send raw userInput (potentially erroneous) to MapBox to retrieve location's lat/lon.
-//  2. GET: Use mapBox's lat/lon to get daily (one week) weather data from Foreca API.
+//  2. Multiple GET: Use mapBox's lat/lon to get weather data (at preferred the time interval) from Foreca API.
 
 let locationInfo
 const weatherInfo = {}
 const mapBoxBaseUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places'
-const forecaBaseUrl = 'https://fnw-us.foreca.com/api/v1/api/v1/'
+const forecaBaseUrl = 'https://fnw-us.foreca.com/api/v1/api/v1'
 
+// 1. GET: Send raw userInput (potentially erroneous) to MapBox to retrieve location's lat/lon.
+//    Performs an API call to MapBox API with userInput.
+//    Sets the response as locationData
+//    Returns a promise.
+const getLocationDetails = url => {
+  return axios.get(url).then(response => {
+    // Filter out empty responses. Mapbox will still respond with 200's on some bad calls.
+    if (response.data.features !== []) {
+      // Set the appropriate response data to locationInfo.
+      locationInfo = {
+        lon: response.data.features[0].center[0],
+        lat: response.data.features[0].center[1],
+        placeName: response.data.features[0].place_name,
+      }
+    }
+  })
+}
+
+// 2. Multiple GET: Use mapBox's lat/lon to get weather data (at preferred the time interval) from Foreca API.
+//    Performs an API call to Foreca by requested time interval.
+//    Sets the response as a key on weatherInfo.
+//    Returns a promise.
 const getWeatherDetails = (timeFrame, axiosConfig) => {
   const { lon, lat, placeName } = locationInfo
   let keyName
   return axios
     .get(`${forecaBaseUrl}/${timeFrame}/${lon},${lat}`, axiosConfig)
     .then(response => {
+      // Prepare the keyName for proper object construction
       timeFrame.includes('forecast/')
         ? (keyName = timeFrame.slice(9, timeFrame.length))
         : null
       timeFrame.includes('forecast/15')
         ? (keyName = timeFrame.slice(11, timeFrame.length))
         : null
+      // Construct the weatherInfo object
       response.data.forecast
         ? (weatherInfo[keyName] = response.data.forecast)
         : (weatherInfo[timeFrame] = response.data.current)
@@ -45,27 +68,16 @@ export default async (req, res) => {
   )}.json?limit=1&access_token=${process.env.MAPBOX_API_KEY}`
 
   // 1. GET: Send raw userInput (potentially erroneous) to MapBox to retrieve location's lat/lon.
-  axios
-    .get(urlComplete)
-    .then(response => {
-      // Filter out empty responses. Mapbox will still respond with 200's on some bad calls.
-      if (response.data.features !== []) {
-        // Receive appropriate placeName from raw userInput.
-        locationInfo = {
-          lon: response.data.features[0].center[0],
-          lat: response.data.features[0].center[1],
-          placeName: response.data.features[0].place_name,
-        }
-      }
-    })
+  getLocationDetails(urlComplete)
     .then(() => {
+      // 2. Multiple GET: Use mapBox's lat/lon to get weather data (at preferred the time interval) from Foreca API.
       Promise.all([
         getWeatherDetails('current', axiosConfig),
         getWeatherDetails('forecast/15minutely', axiosConfig),
         getWeatherDetails('forecast/hourly', axiosConfig),
         getWeatherDetails('forecast/daily', axiosConfig),
       ])
-        .then(() => res.json({ locationInfo, weatherInfo }))
+        .then(() => res.json({ locationInfo, weatherInfo })) // Send all collected information back to the frontend
         .catch(error => console.error('ERROR in Promise.all(): ', error))
     })
     .catch(error => {
