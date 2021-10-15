eaa82c4a0a11e11a030cb7c9969077f9bde7e5fc
Steven Del Rosario <stevendelro@pm.me>
2021-10-15 15:55:09 -0500

    updated index.js to index.ts

--- pages/api/foreca/auth/index.ts
+++ pages/api/foreca/auth/index.ts
@@ -1,13 +1,13 @@
 import axios from 'axios'
+import type { NextApiRequest, NextApiResponse } from 'next'
 import Cookies from 'universal-cookie'
 
 const user = process.env.FORECA_USER
 const password = process.env.FORECA_PASSWORD
 
 // Figure out how to set either the browser's cookie from back here, or the Authorization header.
-// change all the /api files to .ts
 
-export default async (req, res) => {
+export default async (req: NextApiRequest, res: NextApiResponse) => {
   axios
     .post('https://fnw-us.foreca.com/authorize/token?expire_hours=3', {
       user,
