5e52b8e3330fc4115b0c7a2f7af91c844e9232e3
Steven Del Rosario <stevendelro@pm.me>
2021-10-15 15:55:59 -0500

    properly typed top level index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,37 @@
-import { bindActionCreators, Dispatch, ActionCreator } from 'redux'
+import { bindActionCreators, Dispatch } from 'redux'
 import { connect } from 'react-redux'
 import Container from '@material-ui/core/Container'
 import React, { useState } from 'react'
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
 
 import { Action } from '../store/actions'
 import { fetchData } from '../store/action-creators'
 
 // setup a dropdown to choose what language weather data should come back in
 
-type FetchData = {
-  fetchData: (userInput: string) => void
+interface IndexProps {
+  fetchData(userInput: string): Function
 }
 
-const Index = ({ fetchData }: FetchData) => {
+const Index = ({ fetchData }: IndexProps): JSX.Element => {
   const [userInput, setUserInput] = useState('')
 
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
     event.preventDefault()
     fetchData(userInput)
-
     setUserInput('')
   }
 
   return (
     <Container maxWidth='sm'>
-      <h1>index.js</h1>
       <form onSubmit={onSubmit}>
-        <input
-          type='text'
+        <TextField
+          label='Enter Location'
           value={userInput}
           onChange={e => setUserInput(e.target.value)}
         />
-        <button type='submit'>Submit</button>
+        <Button type='submit'>Submit</Button>
       </form>
     </Container>
   )
